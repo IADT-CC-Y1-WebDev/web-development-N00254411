03ed662b0b4f5104d21a62fa91bceb07ec5731d5
Victoria Connolly <N00254411@iadt.ie>
2026-03-11 10:08:19 +0000

    more validation

--- src/exercises/09-js-form-validation/01-contact.js
+++ src/exercises/09-js-form-validation/01-contact.js
@@ -1,1 +1,18 @@
-console.log("hello");
+let submitBtn = document.getElementById('submit_btn');
+let commentForm = document.getElementById('comment_form');
+let nameInput = document.getElementById('name');
+
+let nameError = document.getElementById("name_error")
+
+submitBtn.addEventListener('click', onSubmitForm);
+
+function onSubmitForm(evt) {
+    console.log(evt);
+    evt.preventDefault();
+
+    const name= nameInput.value.trim();
+    if (name === ''){
+        console.log("Name is required")
+        nameError.innerHTML = "Name is required"
+    }
+}
